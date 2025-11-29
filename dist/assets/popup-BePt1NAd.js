31faaac5bed2171e0e4ca4063d2d7d57b771eae5
jack ning <github@bytedesk.com>
2025-11-29 17:20:59 +0800

    chore: update admin-clone dist admin-clone-dist-20251129-172048.zip

---
+++ dist/assets/popup-BePt1NAd.js
@@ -0,0 +1,2 @@
+import{c as t,j as e,R as o,C as r,z as s,A as a}from"./index-3ljul4ma.js";import"./dom-utils-DQVJ0pwT.js";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{locale:s,children:e.jsx(a,{})})}));
+//# sourceMappingURL=popup-BePt1NAd.js.map
---
+++ dist/assets/sidepanel-DRZpAucZ.js
@@ -0,0 +1,2 @@
+import{c as t,j as e,R as o,C as r,z as s,A as a}from"./index-3ljul4ma.js";import"./dom-utils-DQVJ0pwT.js";document.body.dataset.surface="sidepanel";t.createRoot(document.getElementById("root")).render(e.jsx(o.StrictMode,{children:e.jsx(r,{locale:s,children:e.jsx(a,{})})}));
+//# sourceMappingURL=sidepanel-DRZpAucZ.js.map
